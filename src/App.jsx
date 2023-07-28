340d41bd4b82aa86d36b6ae4337fc6ea41a8aeca
MacreenSmith <macreensmith@gmail.com>
2023-07-28 10:25:01 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Form from "./components/Form"
+import MovieDisplay from "./components/MovieDisplay"
+import {useState, useEffect} from 'react'
+import { styled } from "styled-components"
+
+function App() {
+
+  const apiKey = '98e3fb1f'
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+      const data = await response.json();
+      // console.log(data)
+      setMovie(data)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovie('')
+  }, [])
+  return (
+    <>
+     <Form movieSearch={getMovie}/>
+     <MovieDisplay movie={movie}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MovieDisplay.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const Container = styled.div`
+  width: 80%;
+  margin: auto;
+  text-align: center;
+`
+
+const Title = styled.h1`
+  font-size: 3em;
+  color: red;`
+
+
+
+function MovieDisplay ({movie}) {
+
+  const loaded = () => {
+    return (
+      <>
+        <Title>{movie.Title}</Title>
+      <h2>{movie.Genre}</h2>
+      <img src={movie.Poster} alt ={movie.Title}/>
+      <h2>{movie.Year}</h2>
+      </>
+    );
+  };
+
+  const loading = () => {
+    return <h1>No Movie to Display.</h1>
+  }
+  return movie? loaded() : loading()
+}
+  
+
+export default MovieDisplay
